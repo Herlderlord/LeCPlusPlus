29bc95ecc348fd87122313e2f1287e2bbcabca97
Quillot Mathias <mathias.quillot@alumni.univ-avignon.fr>
2021-04-18 11:33:40 +0200

    Sommaire du cours sur les pointeurs

---
+++ exemple.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    /**************************************************************************/
+    /** X - Sommaire **********************************************************/
+    /**************************************************************************/
+    cout << "Ce programme est dédié à l'apprentissage des pointeurs." << endl;
+    cout << "Pour cela, nous allons voir les notions suivantes : " << endl;
+    cout << "1- les adresses des variables" << endl;
+    cout << "2- passage par référence" << endl;
+    cout << "3- pointeur vers une variable" << endl;
+    cout << "4- pointeur vers un tableau" << endl;
+    cout << "5- double pointeur" << endl;
+    cout << end;
+    cout << "Note 1 : Prenez bien le temps de tout lire, toutes les notions abordés ici sont intimement liées." << endl;
+    cout << "Note 2 : Toutes les "
+    cout << endl;
+}
